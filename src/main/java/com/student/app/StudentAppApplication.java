5b20677e500cad2e1856c485ccf034542f8c9e9c
Jochizan <remnyachizot2015@gmail.com>
2021-10-06 22:43:26 -0500

    Iniciando aplicación con spring y docker

---
+++ src/main/java/com/student/app/StudentAppApplication.java
@@ -0,0 +1,21 @@
+package com.student.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class StudentAppApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentAppApplication.class, args);
+    }
+
+}
